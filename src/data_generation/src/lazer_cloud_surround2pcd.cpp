ad11880d9a8992dd54cf0e4544ec82ba4db59859
Han Sun <okam.sun@gmail.com>
2021-10-10 09:50:25 +1100

    listener to the constrcued point cloud, save the data to pcd files

---
+++ src/data_generation/src/lazer_cloud_surround2pcd.cpp
@@ -0,0 +1,29 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_ros/point_cloud.h>
+#include <string>
+
+void laser_cloud_surround_callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    pcl::fromROSMsg(*msg, cloud);
+    ROS_INFO("I heard [%d] Points", cloud.points.size());
+    
+    if (cloud.points.size() > 100000)
+    {   
+        //std::string fielName = "/Data/Robotcar_radar/Scenes/" + std::to_string(msg->header.stamp.sec) + ".pcd";
+        std::string fielName = std::to_string(cloud.points.size()) + ".pcd";
+        pcl::io::savePCDFileASCII(fielName, cloud);
+    }
+}
+
+int main (int argc, char** argv)
+{
+    ros::init(argc, argv, "surroundcloud2pcd_listener");
+
+    ros::NodeHandle n;
+
+    ros::Subscriber sub = n.subscribe("/laser_cloud_surround", 2, laser_cloud_surround_callback);
+
+    ros::spin();
+}
